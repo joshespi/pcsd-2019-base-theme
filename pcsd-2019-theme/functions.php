5a6b9f8fc8ef1eb712a9f37af65bbe09b11d5df1
Josh Espinoza <joshespi@gmial.com>
2019-04-11 16:21:54 -0600

    added version number for scripts file

--- pcsd-2019-theme/functions.php
+++ pcsd-2019-theme/functions.php
@@ -9,7 +9,7 @@ function theme_specific_stylesheet() {
 	/*   CALL ALL CSS AND SCRIPTS FOR SITE */
 	//wp_enqueue_style( 'style', get_stylesheet_uri());
 	wp_enqueue_script('slick-script', get_template_directory_uri() .'/assets/slick/slick.min.js', array('jquery'), null, true);
-	wp_enqueue_script('my-custom-scripts', get_template_directory_uri() .'/assets/js/scripts.js', array('jquery', 'slick-script',), null, true);
+	wp_enqueue_script('my-custom-scripts', get_template_directory_uri() .'/assets/js/scripts.js', array('jquery', 'slick-script',), '1.0.1', true);
 	if ( is_404() ) {
 		wp_enqueue_script( '404easterEgg');
 	}
