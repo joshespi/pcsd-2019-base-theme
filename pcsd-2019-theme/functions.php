0ff14510b19311d85bf0cd26df201097051ef225
Josh Espinoza <joshespi@gmial.com>
2020-01-27 09:25:06 -0700

    changed the tutorials page to pull from a global page

--- pcsd-2019-theme/functions.php
+++ pcsd-2019-theme/functions.php
@@ -7,7 +7,7 @@ function theme_specific_stylesheet() {
 	wp_register_script( '404easterEgg', get_template_directory_uri() .'/assets/js/404.js', '', '', true );
 
 	/*   CALL ALL CSS AND SCRIPTS FOR SITE */
-	wp_enqueue_style( 'parent-style', get_stylesheet_uri(), '', '1.4', false);
+	wp_enqueue_style( 'parent-style', get_stylesheet_uri(), '', '1.5', false);
 	wp_enqueue_script('slick-script', get_template_directory_uri() .'/assets/slick/slick.min.js', array('jquery'), null, true);
 	wp_enqueue_script('my-custom-scripts', get_template_directory_uri() .'/assets/js/scripts.js', array('jquery', 'slick-script',), '1.0.1', true);
 	if ( is_404() ) {
@@ -108,34 +108,14 @@ function pcsd_tut_admin_menu() {
 	add_menu_page( 'Tutorials Dashboard', 'Tutorials', 'read', 'pcsd_tutorial-admin-page.php', 'pcsd_tutorial_admin_page', 'dashicons-media-default', 4  );
 }
 function pcsd_tutorial_admin_page(){
-	?>
-		<div class="wrap">
-			<h2>Tutorials For Wordpress</h2>
-			<h3>Posts &amp; Pages</h3>
-				<ul>
-					<li><a href="https://docs.google.com/document/d/1j2QgAOSgl_n8bugW_BObjKolz406czLohKQaIz7hMXU/edit?usp=sharing">New Post Instructions</a></li>
-					<li><a href="https://docs.google.com/document/d/1AQ5k7Bir2QpcE1jisJffAonz463doPMstUBBnDvgo9c/edit?usp=sharing">New Announcement Instructions</a></li>
-					<li><a href="https://provo.edu/wp-content/uploads/2017/08/08-18-2017-formatting-your-post.pdf">Formatting Your Post</a></li>
-					<li><a href="https://provo.edu/wp-content/uploads/2017/08/08-18-201-adding-external-links.pdf">Adding External Links</a></li>
-					<li><a href="https://provo.edu/wp-content/uploads/2019/02/An-Explanation-of-Website-Standards.pdf">An Explanation of Website Standards</a></li>
-					<li><a href="https://docs.google.com/document/d/1mCYeM9SHFtToaCN-YaWeAdCovRNrqs5GCfmTGLr04tU/edit?usp=sharing">Creating and Using Classroom Links</a></li>
-				</ul>
-			<h3>Images &amp; Documents</h3>
-				<ul>
-					<li><a href="https://docs.google.com/document/d/1TPIqkKVvY_qbxBeTRDhXkbKvvFF8uF0yyoqIgxGtuc0/edit?usp=sharing">How to Size Your Image</a></li>
-					<li><a href="https://provo.edu/wp-content/uploads/2018/12/12-12-2018-image-resources-updated-for-2019-theme.pdf">Image Resources</a></li>
-					<li><a href="https://provo.edu/wp-content/uploads/2017/08/08-18-2017-adding-pdfs.pdf">Adding PDFs</a></li>
-					<li><a href="https://provo.edu/wp-content/uploads/2017/08/08-18-2017-file-naming-standards.pdf">File Naming Standards</a></li>
-					<li><a href="https://docs.google.com/document/d/1P-ZmnvAQ1mymrFX6DvxjQxdD2Y3X9bAMz2rm5wnd9lY/edit?usp=sharing">Adding Videos</a></li>
-					<li><a href="https://docs.google.com/document/d/1J418MQ561-Vo1xQH4s53pkAqlrcC5RMHQQxlcA51bhM/edit?usp=sharing">Adding a Gallery</a></li>
-					
-				</ul>
-			<h3>Teacher Tutorials</h3>
-				<ul>
-					<li><a href="https://provo.edu/wp-content/uploads/2017/08/Secondary-School-Teachers-Add-a-Syllabus-to-Your-Official-Web-Page.pdf">How to Add a Syllabus to Your Official Web Page</a></li>
-				</ul>
-		</div>
-	<?php
+		$tuts_page = curl_init();
+		// set URL and other appropriate options
+		curl_setopt($tuts_page, CURLOPT_URL, 'https://globalassets.provo.edu/globalpages/tutorials-page.php');
+		curl_setopt($tuts_page, CURLOPT_HEADER, 0);			
+		// grab URL and pass it to the browser
+		curl_exec($tuts_page);	
+		// close cURL resource, and free up system resources
+		curl_close($tuts_page);
 }
 /*==========================================================================================
 Tracks User Last Login
